05e4a19a7e6941aa2f259c487cc03aa1e252b2eb
nil2mahadik <nil2mahadik@gmail.com>
2015-01-13 14:01:31 +0530

    commit

--- IssueManager/src/main/java/com/cts/issuemanager/controllers/IssueController.java
+++ IssueManager/src/main/java/com/cts/issuemanager/controllers/IssueController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+// controller to handle web requests
 @Controller
 public class IssueController {
 	@RequestMapping(value="/issue",method=RequestMethod.GET)
